530d39e93b89ab70cb4a93e83090d57f04176b94
Kellen Karls <kellen.karls@drake.edu>
2021-02-24 10:16:37 -0600

    Ran black to reformat code

--- iroom_api/main.py
+++ iroom_api/main.py
@@ -4,34 +4,36 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
-#Builder.load_file('widgets.kv')
+# Builder.load_file('widgets.kv')
+
 
 class LoginScreen(GridLayout):
-
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='iRoom'))
-        self.add_widget(Label(text='help'))
-        self.add_widget(Label(text='User Name'))
+        self.add_widget(Label(text="iRoom"))
+        self.add_widget(Label(text="help"))
+        self.add_widget(Label(text="User Name"))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
+        self.add_widget(Label(text="password"))
         self.password = TextInput(password=True, multiline=False)
         self.add_widget(self.password)
-        self.add_widget(Label(text='Log in'))
+        self.add_widget(Label(text="Log in"))
+
 
 class Background(Widget):
     pass
 
+
 class HomeScreen(Widget):
     pass
 
-class MyApp(App):
 
+class MyApp(App):
     def build(self):
         return HomeScreen()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyApp().run()
