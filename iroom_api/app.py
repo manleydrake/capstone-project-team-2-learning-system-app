54149ff77a9c2b71f7fe4cdde6b9d9708dac44c2
Kellen Karls <kellen.karls@drake.edu>
2021-03-16 17:51:10 -0500

    Tried hooking Register new user up... almost got it lol

--- iroom_api/app.py
+++ iroom_api/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
-
+    return render_template("home.html")
 
 @app.route("/home.html")
 def display_home():
@@ -22,8 +22,24 @@ def display_register():
 
 @app.route("/course1.html")
 def display_courses():
-    return render_template("courses.html")
+    return render_template("courses.html", class_name=class_name)
 
 @app.route("/classes.html")
 def display_classes():
     return render_template("classes.html")
+
+@app.route("/new_user.html")#/new_user.html?email=a&username=d&password=12345678&student_prof=s
+def register_new_user(username, email, password, student_prof):
+    add_user(username, email, password, student_prof)
+    return render_template("home.html", user=user)
+
+def read_users_csv():
+    with open('database/users.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+def add_user(username, email, password, student_prof):
+    with open('database/users.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        #Read last row and determine user_id
+        writer.writerow(user_id, username, email, password, student_prof)
