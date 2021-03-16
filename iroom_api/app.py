2bbf45f8d6356843dd957c87803de1f2bc1f0aca
Kellen Karls <kellen.karls@drake.edu>
2021-03-16 13:53:33 -0500

    worked on website

--- iroom_api/app.py
+++ iroom_api/app.py
@@ -9,6 +9,18 @@ def hello_world():
     return "Hello, World!"
 
 
-@app.route("/home")
+@app.route("/home.html")
 def display_home():
     return render_template("home.html")
+
+@app.route("/login.html")
+def display_login():
+    return render_template("login.html")
+
+@app.route("/register.html")
+def display_register():
+    return render_template("register.html")
+
+@app.route("/course1.html")
+def display_courses():
+    return render_template("courses.html")
