3692ab032bde7dc338083207bb08a824004f6d6a
Kellen Karls <kellen.karls@drake.edu>
2021-03-29 12:38:32 -0500

    Added logging in and password hashing

--- iroom_api/app.py
+++ iroom_api/app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 import csv
 import random
+import hashlib
 
 app = Flask(__name__)
 
@@ -36,17 +37,40 @@ def register_new_user():
     email = request.form['email']
     password = request.form['password']
     student_prof = request.form['student_prof']
-    add_user(username, email, password, student_prof)
+    add_user(username, email, hash_password(password), student_prof)
     return render_template("home.html",user=username)
+
+@app.route("/login_user.html", methods=["post"])
+def login_user():
+    username = request.form['username']
+    password = request.form['password']
+    valid_log_in = is_login_valid(username, hash_password(password))
+    if valid_log_in:
+        return render_template("home.html", user=username)
+    else:
+        return render_template("login.html", error="invalid username or password")
 
 def read_users_csv():
     with open('database/users.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 
 def add_user(username, email, password, student_prof):
-    with open('database/users.csv', 'w', newline='') as csvfile:
+    with open('database/users.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         #Read last row and determine user_id or assign random userID
         user_id = random.randint(1000000, 9999999)
         writer.writerow([user_id, username, email, password, student_prof])
+
+def is_login_valid(username, password):
+    with open('database/users.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        valid = False
+        for row in reader:
+            if(username in row and password in row):
+                valid = True
+        return valid
+
+def hash_password(password):
+    h = hashlib.pbkdf2_hmac('sha256', b'password', b'salt', 100000)
+    return h.hex()
