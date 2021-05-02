6574d476b25e663b4b6455ea79494b4ebba37f1c
Kellen Karls <kellen.karls@drake.edu>
2021-05-02 15:42:22 -0500

    Added sessions so users can stay logged in. Based courses shown off of logged in user. Added logout and course search

--- iroom_api/app.py
+++ iroom_api/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import Flask, session
+#from flask.ext.session import Session
 import csv
 import random
 import hashlib
 
 app = Flask(__name__)
+
+app.secret_key = "super_secret_key123"
+
+#SESSION_TYPE = 'redis'
+#app.config.from_object(__name__)
+#session(app)
 
 @app.route("/")
 def hello_world():
@@ -29,7 +37,24 @@ def display_courses():
 
 @app.route("/classes.html")
 def display_classes():
-    return render_template("classes.html")
+    #We need a list of class names returned based on the logged in user
+    with open('database/users.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            if(session['username'] in row):
+                course_ids = row[5:]
+    course_names = []
+    with open('database/courses.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            for id in course_ids:
+                if(id == row[0]):
+                    course_names.append(row[1])
+    return render_template("classes.html", course_names=course_names)
+
+@app.route("/user_page.html")
+def display_user_page():
+    return render_template("user_page.html")
 
 @app.route("/new_user.html", methods=["post"])
 def register_new_user():
@@ -38,17 +63,56 @@ def register_new_user():
     password = request.form['password']
     student_prof = request.form['student_prof']
     add_user(username, email, hash_password(password), student_prof)
-    return render_template("home.html",user=username)
+    return render_template("home.html")
+
+@app.route("/logout.html", methods=["post"])
+def logout():
+    session.pop('username', default=None)
+    return render_template("home.html")
 
 @app.route("/login_user.html", methods=["post"])
 def login_user():
     username = request.form['username']
     password = request.form['password']
     valid_log_in = is_login_valid(username, hash_password(password))
+    SESSION_TYPE = 'redis'
+    app.config.from_object(__name__)
+    #session(app)
     if valid_log_in:
-        return render_template("home.html", user=username)
+        session['username'] = username
+        return render_template("home.html")
     else:
         return render_template("login.html", error="invalid username or password")
+
+@app.route("/course_search.html", methods=["post"])
+def course_search():
+    search = request.form['course_search']
+    with open('database/courses.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        possible_courses = []
+        for row in reader:
+            if(search.lower() in row[1].lower() and row[1] != "course_name"):
+                temp = [row[0], row[1]]
+                possible_courses.append(temp)
+    return render_template("user_page.html", possible_courses=possible_courses)
+
+@app.route("/add_course.html", methods=["post"])
+def add_course():
+    course_id = request.form['course_to_add']
+    with open('database/users.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        lines = []
+        for row in reader:
+            if(session['username'] in row):
+                row.append(course_id)
+                lines.append(row)
+            else:
+                lines.append(row)
+    with open('database/users.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerows(lines)
+    return render_template("/home.html")
+
 
 def read_users_csv():
     with open('database/users.csv', newline='') as csvfile:
